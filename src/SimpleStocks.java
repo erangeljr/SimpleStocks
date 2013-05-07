cf3b1d6e87f1b9a3c7342807eee3e9a0945112a6
erangeljr <hacker@eddierangel.com>
2013-05-06 20:35:55 -0700

    Added Additional Threads
    Added StockDataThreadOne and StockDataThreadTwo.
    Added run methods for Threads.
    Modifying Colors of lines being drawn.

--- src/SimpleStocks.java
+++ src/SimpleStocks.java
@@ -13,6 +13,12 @@ public class SimpleStocks extends JApplet implements ActionListener{
     int	   data[] = new int [SortCons.ARRAY_SIZE];
 	
     GenerateDataThread generateDataThread = null;
+    StockDataThreadOne stockDataThreadOne = null;
+    StockDataThreadTwo stockDataThreadTwo = null;
+    
+    
+//    ShuffleDataThread  shuffleDataThread = null;
+//    MergeSortThread    mergeSortThread = null;
     
     public SimpleStocks(){
     	//Default Constructor
@@ -38,13 +44,200 @@ public class SimpleStocks extends JApplet implements ActionListener{
        }
     
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		int k = -1;
+		Object o = e.getSource();
+		for ( int i = 0; i < buttonArray.length; i++ ) {
+			if ( o == buttonArray[i] ) { 
+				k = i; 
+				break; }
+		}
+
+		switch ( k ) {
+			case 0: 
+				generateDataThread = new GenerateDataThread(
+						stockDataThreadOne, stockDataThreadTwo, data);
+				generateDataThread.start();
+				for ( int i = 1; i < 3; i++) buttonArray[i].setEnabled(true) ;
+				break;
+			case 1: 
+//				mergeSortThread = new MergeSortThread (
+//				generateDataThread, shuffleDataThread, data);
+//				mergeSortThread.start() ; 
+				break;
+			case 2: 
+//				shuffleDataThread = new ShuffleDataThread (
+//				generateDataThread, mergeSortThread, data);
+//				shuffleDataThread.start(); 
+				break;
+		}
+	}
+	
+	 public static void main( String argv[] ) {
+			JFrame fmObj = new JFrame("Simple Stock Simulation");
+			fmObj.addWindowListener( new  WindowAdapter() {
+					public void windowClosing( WindowEvent e ) { System.exit(0); } } );
+			fmObj.setSize(600, 500);
+			SimpleStocks apObj = new SimpleStocks();
+			apObj.init();
+			fmObj.getContentPane().add(apObj);
+			fmObj.setVisible(true);
+			apObj.start();
+		   }
+
+}
+
+class GenerateDataThread extends Thread {
+	int [] data;
+	   Thread thrd1 = null, thrd2 = null;
+
+	   public GenerateDataThread(Thread t1, Thread t2, int [] d ) {
+		   thrd1 = t1; 
+		   thrd2 = t2; 
+		   data = d;
+	   }
+
+	   public void run() {
+		Random rnd = new Random();
+		try { 
+			if ( thrd1 != null ) 
+				thrd1.join(); 
+			if (thrd2 != null ) 
+				thrd2.join();  }
+		catch (InterruptedException e) {}
+		for ( int i = 0; i < data.length; i ++ )
+			data[i] = rnd.nextInt( SortCons.MAX_INT );
+		DrawLinesThread t = new DrawLinesThread( data, 0, data.length );
+		t.start();
+		try { 
+			t.join(); 
+		} 
+		catch (InterruptedException e) {}
+	   }
+}
+
+class StockDataThreadOne extends Thread {
+	int [] data;
+	Thread thrd1 = null, thrd2 = null;
+	
+	public StockDataThreadOne(Thread t1, Thread t2, int [] d ){
+		thrd1 = t1; 
+		thrd2 = t2; 
+		data = d;
 		
 	}
-
-}
-
-class GenerateDataThread{
+	
+	public void run() {
+		Random rnd = new Random();
+		try { 
+			if ( thrd1 != null ) 
+				thrd1.join(); 
+			if (thrd2 != null ) 
+				thrd2.join();  }
+		catch (InterruptedException e) {}
+		for ( int i = 0; i < data.length; i ++ )
+			data[i] = rnd.nextInt( SortCons.MAX_INT );
+		DrawLinesThread t = new DrawLinesThread( data, 0, data.length );
+		t.start();
+		try { 
+			t.join(); 
+		} 
+		catch (InterruptedException e) {}
+	   }
+	
 	
 }
+
+class StockDataThreadTwo extends Thread {
+	int [] data;
+	Thread thrd1 = null, thrd2 = null;
+	
+	public StockDataThreadTwo(Thread t1, Thread t2, int [] d ){
+		thrd1 = t1; 
+		thrd2 = t2; 
+		data = d;
+	}
+	
+	public void run() {
+		Random rnd = new Random();
+		try { 
+			if ( thrd1 != null ) 
+				thrd1.join(); 
+			if (thrd2 != null ) 
+				thrd2.join();  }
+		catch (InterruptedException e) {}
+		for ( int i = 0; i < data.length; i ++ )
+			data[i] = rnd.nextInt( SortCons.MAX_INT );
+		DrawLinesThread t = new DrawLinesThread( data, 0, data.length );
+		t.start();
+		try { 
+			t.join(); 
+		} 
+		catch (InterruptedException e) {}
+	   }
+}
+
+class DrawLinesThread extends Thread {
+	   int data[], lb, ub;
+	   Color rectColor = Color.yellow, lineColor = Color.white, backgroundColor = Color.black;
+	   Canvas cvs = SimpleStocks.drawingBoard;
+	   Graphics2D g2d;
+
+	   String title = "Simulate Stock Market Data";
+	   FontMetrics  fm;
+
+	   DrawLinesThread( int [] d, int l, int u ) {
+		data = d; lb = l; ub = u; g2d = (Graphics2D) cvs.getGraphics();
+		g2d.setFont( new Font("TimesRoman", Font.BOLD, 14));
+	   }
+
+	   public void run() {
+		int cvsWidth = cvs.getWidth(), cvsHeight = cvs.getHeight();
+		int drawingWidth = cvs.getWidth() - SortCons.RIGHT_INSET - SortCons.LEFT_INSET;
+	 	int drawingHeight = cvs.getHeight() - SortCons.TOP_INSET - SortCons.BOTTOM_INSET;
+		float gap = (float) drawingWidth / (SortCons.ARRAY_SIZE + 1);
+
+		// g2d.clearRect(0, 0, drawingWidth, drawingHeight);
+		g2d.setColor(rectColor);
+		g2d.drawRect( SortCons.LEFT_INSET, SortCons.TOP_INSET, drawingWidth, drawingHeight);
+		fm = g2d.getFontMetrics();
+		g2d.drawString(title, (cvsWidth - fm.stringWidth(title)) / 2, SortCons.TOP_INSET-5);
+//		if ( lb == 0 && ub == SortCons.ARRAY_SIZE ) 
+//			lineColor = generateColor();
+//		else 
+//			lineColor = Color.white;
+
+		for ( int i = lb; i < ub; i++ ) {
+			// Erase a line
+			g2d.setColor(backgroundColor);
+			g2d.drawLine((int ) ( (i + 1)  * gap + SortCons.LEFT_INSET),
+				cvsHeight - SortCons.BOTTOM_INSET,
+				(int ) ( (i + 1)  * gap + SortCons.LEFT_INSET), 
+				cvsHeight - SortCons.BOTTOM_INSET -  drawingHeight + 1 );
+			// draw a line.
+			if ( (lb != 0 || ub != SortCons.ARRAY_SIZE) && ( i == ub - 1 || i == SortCons.ARRAY_SIZE - 1))
+				g2d.setColor(Color.magenta);
+			else 
+				g2d.setColor( generateColor());
+
+			g2d.drawLine((int ) ( (i + 1)  * gap + SortCons.LEFT_INSET),
+				cvsHeight - SortCons.BOTTOM_INSET,
+				(int ) ( (i + 1)  * gap + SortCons.LEFT_INSET), 
+				cvsHeight - SortCons.BOTTOM_INSET - 
+				(int) ((float) data[i] / SortCons.MAX_INT * drawingHeight) );
+			try { 
+				sleep( data.length * 5 / (ub - lb)); 
+			} 
+			catch (InterruptedException e) {}
+		}
+	   }
+
+	   Color generateColor() {
+		float r = 0, g = 0, b = 0;
+		while ( r+g+b < 0.5 )  {
+			r = (float) Math.random(); g = (float) Math.random(); b = (float) Math.random(); }
+		return new Color(r, g, b);
+	  }
+		
+	}
