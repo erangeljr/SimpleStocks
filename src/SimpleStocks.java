1c3d15cd150f102c8d4628f9dbd5bf46d4c9ddb4
erangeljr <hacker@eddierangel.com>
2013-05-06 18:02:42 -0700

    Extends and Implements
    extends JApplet implements ActionListener
    
    Added interfaces methods.

--- src/SimpleStocks.java
+++ src/SimpleStocks.java
@@ -1,4 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 
-public class SimpleStocks {
+public class SimpleStocks extends JApplet implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
